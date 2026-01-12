f8ab54a08dfe10b29f04186fdd8c8a0b30d86a6a
otaviouu <otavio.venancio.oliveira@usp.br>
2026-01-12 13:20:56 -0300

    feat: Integrate user authentication into router and root layout

--- lib/blogex_web/router.ex
+++ lib/blogex_web/router.ex
@@ -1,6 +1,8 @@
 defmodule BlogexWeb.Router do
   use BlogexWeb, :router
 
+  import BlogexWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule BlogexWeb.Router do
     plug :put_root_layout, html: {BlogexWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -40,5 +43,33 @@ defmodule BlogexWeb.Router do
       live_dashboard "/dashboard", metrics: BlogexWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", BlogexWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{BlogexWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", BlogexWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{BlogexWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
