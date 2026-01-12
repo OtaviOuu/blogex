f7289ba37b7785d893b25e28956ca4eddf0fee64
otaviouu <otavio.venancio.oliveira@usp.br>
2026-01-12 13:20:56 -0300

    feat: Create database migrations for posts context tables with author link

---
+++ priv/repo/migrations/20260112160326_create_posts_context_tables.exs
@@ -0,0 +1,31 @@
+defmodule Blogex.Repo.Migrations.CreatePostsContextTables do
+  use Ecto.Migration
+
+  def change do
+    create table(:posts, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :title, :string, null: false
+      add :content, :text, null: false
+      add :author_id, references(:users, type: :binary_id), null: false
+
+      timestamps()
+    end
+
+    create table(:tags) do
+      add :name, :string, null: false
+
+      timestamps()
+    end
+
+    create unique_index(:tags, [:name])
+
+    create table(:post_tags, primary_key: false) do
+      add :post_id, references(:posts, type: :binary_id), null: false
+      add :tag_id, references(:tags), null: false
+
+      timestamps()
+    end
+
+    create unique_index(:post_tags, [:post_id, :tag_id])
+  end
+end
